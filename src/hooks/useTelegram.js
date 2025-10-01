faa3ce9fd7851cee5fcf79154e3d3eba0500b2de
grechcode <147876635+grechcode@users.noreply.github.com>
2025-10-01 17:33:22 +0500

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -101,7 +101,7 @@ export const useTelegram = () => {
       tg.MainButton.show();
     }
     return () => {
-      tg.MainButton.setText("");
+      tg.MainButton.setText(MENU_MAIN_BUTTON_TEXT);
       tg.BackButton.offClick(backButtonHandler);
       tg.MainButton.offClick(menuMainButtonHandler);
       tg.MainButton.offClick(cartMainButtonHandler);
