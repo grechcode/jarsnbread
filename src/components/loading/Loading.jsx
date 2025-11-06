67e2f510fa79465ab9f464f5514ed3efb7764dc3
Gregory <grechka730@mail.ru>
2025-11-06 15:00:57 +0500

    test loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,8 +1,8 @@
 import { cn } from "@/utils";
 import styles from "./loading.module.css";
 
-export const Loading = ({ contentLoaded }) => (
-  <div className={cn(styles.loading, contentLoaded && styles.hide)}>
+export const Loading = ({ isImgsLoaded }) => (
+  <div className={cn(styles.loading, isImgsLoaded && styles.hide)}>
     <div className={styles.logo}>
       <div className={styles.cap} />
       <span className={styles.jarText}>БАНОЧКИ</span>
--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -1,38 +1,48 @@
 import { useEffect, useState } from "react";
 
 export const useLoading = () => {
-  const [contentLoaded, setContentLoaded] = useState(false);
+  const [isImgsLoaded, setIsImgsLoaded] = useState(false);
+  const [isFontsLoaded, setIsFontsLoaded] = useState(false);
+  const [isAnimationLoad, setIsAnimationLoad] = useState(false);
   const [imagesCount, setImagesCount] = useState(null);
   const [loadedCount, setLoadedCount] = useState(0);
 
   useEffect(() => {
-    // const allImages = document.querySelectorAll("img");
-    // setImagesCount(allImages.length);
-
-    // const loadCounter = () => setLoadedCount((prev) => prev + 1);
-
-    // allImages.forEach((img) => {
-    //   img.addEventListener("load", loadCounter);
-    // });
-
-    // return () => {
-    //   allImages.forEach((img) => {
-    //     img.removeEventListener("load", loadCounter);
-    //   });
-    // };
-
-    const timeout = setTimeout(() => {
-      setImagesCount(11);
-      setLoadedCount(10);
-    }, 3000);
-    return () => clearTimeout(timeout);
+    document.fonts.ready.then(() => {
+      setIsFontsLoaded(true);
+    });
   }, []);
 
   useEffect(() => {
-    if (imagesCount === loadedCount) {
-      setContentLoaded(true);
-    }
-  }, [imagesCount, loadedCount]);
+    if (!isFontsLoaded) return;
+    const timeout = setTimeout(() => {
+      setIsAnimationLoad(true);
+    }, 4000);
+    return () => clearTimeout(timeout);
+  }, [isFontsLoaded]);
 
-  return contentLoaded;
+  useEffect(() => {
+    const allImages = document.querySelectorAll("img");
+    setImagesCount(allImages.length);
+
+    const loadCounter = () => setLoadedCount((prev) => prev + 1);
+
+    allImages.forEach((img) => {
+      img.addEventListener("load", loadCounter);
+    });
+
+    return () => {
+      allImages.forEach((img) => {
+        img.removeEventListener("load", loadCounter);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isAnimationLoad && imagesCount === loadedCount) {
+      setIsImgsLoaded(true);
+    }
+  }, [imagesCount, loadedCount, isAnimationLoad]);
+
+  return { isFontsLoaded, isImgsLoaded };
 };
