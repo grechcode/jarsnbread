d8e8283600769b3eade8232e05a6d5c68fa06089
Gregory <grechka730@mail.ru>
2025-11-06 17:49:55 +0500

    test loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,16 +1,14 @@
 import styles from "./loading.module.css";
 
-export const Loading = ({ isFontsLoaded }) => {
+export const Loading = () => {
   return (
     <div className={styles.loading}>
-      {isFontsLoaded && (
-        <div className={styles.logo}>
-          <div className={styles.cap} />
-          <span className={styles.jarText}>БАНОЧКИ</span>
-          <span className={styles.jarBottom}>И</span>
-          <span className={styles.bread}>ХЛЕБ</span>
-        </div>
-      )}
+      <div className={styles.logo}>
+        <div className={styles.cap} />
+        <span className={styles.jarText}>БАНОЧКИ</span>
+        <span className={styles.jarBottom}>И</span>
+        <span className={styles.bread}>ХЛЕБ</span>
+      </div>
     </div>
   );
 };
