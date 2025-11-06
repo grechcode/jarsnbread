f7ec874cf5d461b30300d7743dd371d77ec7fa65
Gregory <grechka730@mail.ru>
2025-11-06 15:00:57 +0500

    test loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import { Loading } from "@/components";
 
 const App = () => {
   const { currentPage } = useAppContext();
-  const contentLoaded = useLoading();
+  const { isFontsLoaded, isImgsLoaded } = useLoading();
 
   useTelegram();
 
   return (
     <div className={styles.content}>
-      <Loading contentLoaded={contentLoaded} />
-      {/* {currentPage === PAGES.menu && <Menu />}
-      {currentPage === PAGES.cart && <Cart />} */}
+      {isFontsLoaded && <Loading isImgsLoaded={isImgsLoaded} />}
+
+      {currentPage === PAGES.menu && <Menu />}
+      {currentPage === PAGES.cart && <Cart />}
     </div>
   );
 };
