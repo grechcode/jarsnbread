8e87ec0b35469e90b4bf069d7c1cf28b7908e1b6
Gregory <grechka730@mail.ru>
2025-11-06 16:12:15 +0500

    test loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,15 +1,33 @@
 import { cn } from "@/utils";
 import styles from "./loading.module.css";
+import { memo, useEffect } from "react";
 
-export const Loading = ({ isImgsLoaded, isFontsLoaded }) => (
-  <div className={cn(styles.loading, isImgsLoaded && styles.hide)}>
-    {isFontsLoaded && (
+export const Loading = ({ isImgsLoaded, isFontsLoaded }) => {
+  const Logo = memo(() => {
+    useEffect(() => {
+      let tg = window.Telegram.WebApp;
+      tg.HapticFeedback.impactOccurred("light");
+      const timeout = setTimeout(() => {
+        tg.HapticFeedback.selectionChanged();
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }, []);
+
+    return (
       <div className={styles.logo}>
         <div className={styles.cap} />
         <span className={styles.jarText}>БАНОЧКИ</span>
         <span className={styles.jarBottom}>И</span>
         <span className={styles.bread}>ХЛЕБ</span>
       </div>
-    )}
-  </div>
-);
+    );
+  });
+
+  return (
+    // <div className={cn(styles.loading, isImgsLoaded && styles.hide)}>
+    //   {isFontsLoaded && <Logo />}
+    // </div>
+    <div className={styles.loading}>{isFontsLoaded && <Logo />}</div>
+  );
+};
