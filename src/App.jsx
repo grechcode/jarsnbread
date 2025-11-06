e86586788db850146971a52b279b67a9d8effc04
Gregory <grechka730@mail.ru>
2025-11-06 16:33:09 +0500

    test loading

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,41 @@ const App = () => {
   useTelegram();
 
   useEffect(() => {
-    if (isFontsLoaded) {
+    const hapticAnimation = () => {
       let tg = window.Telegram.WebApp;
-      tg.HapticFeedback.impactOccurred("heavy");
-      const timeout = setTimeout(() => {
+      setTimeout(() => {
+        tg.HapticFeedback.selectionChanged();
+      }, 1000);
+      setTimeout(() => {
+        tg.HapticFeedback.selectionChanged();
+      }, 1300);
+      setTimeout(() => {
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("light");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("medium");
+        tg.HapticFeedback.impactOccurred("heavy");
+        tg.HapticFeedback.impactOccurred("heavy");
+        tg.HapticFeedback.impactOccurred("heavy");
+        tg.HapticFeedback.impactOccurred("heavy");
+        tg.HapticFeedback.impactOccurred("heavy");
+        tg.HapticFeedback.impactOccurred("heavy");
+      }, 1400);
+      setTimeout(() => {
         tg.HapticFeedback.selectionChanged();
       }, 3000);
+    };
 
-      return () => clearTimeout(timeout);
+    if (isFontsLoaded) {
+      hapticAnimation();
     }
   }, [isFontsLoaded]);
 
