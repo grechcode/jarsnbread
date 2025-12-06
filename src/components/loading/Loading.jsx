935927a4862fba763b0becef512fabcb99e86adc
Gregory <grechka730@mail.ru>
2025-12-06 12:49:08 +0500

    start runningLine feature

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,13 +1,60 @@
+import { cn } from "@/utils";
 import styles from "./loading.module.css";
 
 export const Loading = () => {
+  const JarIcon = () => (
+    <div className={styles.jarIcon}>
+      <div className={styles.iconCap} />
+      <div className={styles.iconJar} />
+    </div>
+  );
+
+  const XIcon = () => (
+    <div className={styles.xIcon}>
+      <div className={styles.firstStick} />
+      <div className={styles.secondStick} />
+    </div>
+  );
+  const BreadIcon = () => (
+    <div className={styles.breadIcon}>
+      <div className={styles.iconBread} />
+    </div>
+  );
+
+  const AnimationLine = ({ animationDirection }) => (
+    <div className={cn(styles.animationLine, styles[animationDirection])}>
+      {Array(6)
+        .fill(null)
+        .map((_, index) => (
+          <>
+            <JarIcon />
+            <XIcon />
+            <BreadIcon />
+            <XIcon />
+          </>
+        ))}
+    </div>
+  );
+
+  const Logo = () => (
+    <div className={styles.logo}>
+      <div className={styles.cap} />
+      <span className={styles.jarText}>БАНОЧКИ</span>
+      <span className={styles.jarBottom}>И</span>
+      <span className={styles.bread}>ХЛЕБ</span>
+    </div>
+  );
+
   return (
     <div className={styles.loading}>
-      <div className={styles.logo}>
-        <div className={styles.cap} />
-        <span className={styles.jarText}>БАНОЧКИ</span>
-        <span className={styles.jarBottom}>И</span>
-        <span className={styles.bread}>ХЛЕБ</span>
+      <div className={styles.upperLine}>
+        <AnimationLine animationDirection="right" />
+        <AnimationLine animationDirection="left" />
+      </div>
+      <Logo />
+      <div className={styles.downerLine}>
+        <AnimationLine animationDirection="right" />
+        <AnimationLine animationDirection="left" />
       </div>
     </div>
   );
