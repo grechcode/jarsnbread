bd56161446dd381d64fb0c8eccae95c80f0be988
Gregory <grechka730@mail.ru>
2025-12-06 13:39:24 +0500

    runningLine test

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -3,56 +3,56 @@ import { cn } from "@/utils";
 import { useLoading } from "@/hooks";
 import { useEffect } from "react";
 
+const JarIcon = () => (
+  <div className={styles.jarIcon}>
+    <div className={styles.iconCap} />
+    <div className={styles.iconJar} />
+  </div>
+);
+
+const XIcon = () => (
+  <div className={styles.xIcon}>
+    <div className={styles.firstStick} />
+    <div className={styles.secondStick} />
+  </div>
+);
+
+const BreadIcon = () => (
+  <div className={styles.breadIcon}>
+    <div className={styles.iconBread} />
+  </div>
+);
+
+const AnimationLine = ({ animationDirection }) => (
+  <div className={cn(styles.animationLine, styles[animationDirection])}>
+    {Array(6)
+      .fill(null)
+      .map((_, index) => (
+        <>
+          <JarIcon />
+          <XIcon />
+          <BreadIcon />
+          <XIcon />
+        </>
+      ))}
+  </div>
+);
+
+const Logo = () => (
+  <div className={styles.logo}>
+    <div className={styles.cap} />
+    <span className={styles.jarText}>БАНОЧКИ</span>
+    <span className={styles.jarBottom}>И</span>
+    <span className={styles.bread}>ХЛЕБ</span>
+  </div>
+);
+
 export const Loading = () => {
   const { hapticAnimation } = useLoading();
 
   useEffect(() => {
     hapticAnimation();
   }, []);
-
-  const JarIcon = () => (
-    <div className={styles.jarIcon}>
-      <div className={styles.iconCap} />
-      <div className={styles.iconJar} />
-    </div>
-  );
-
-  const XIcon = () => (
-    <div className={styles.xIcon}>
-      <div className={styles.firstStick} />
-      <div className={styles.secondStick} />
-    </div>
-  );
-
-  const BreadIcon = () => (
-    <div className={styles.breadIcon}>
-      <div className={styles.iconBread} />
-    </div>
-  );
-
-  const AnimationLine = ({ animationDirection }) => (
-    <div className={cn(styles.animationLine, styles[animationDirection])}>
-      {Array(6)
-        .fill(null)
-        .map((_, index) => (
-          <>
-            <JarIcon />
-            <XIcon />
-            <BreadIcon />
-            <XIcon />
-          </>
-        ))}
-    </div>
-  );
-
-  const Logo = () => (
-    <div className={styles.logo}>
-      <div className={styles.cap} />
-      <span className={styles.jarText}>БАНОЧКИ</span>
-      <span className={styles.jarBottom}>И</span>
-      <span className={styles.bread}>ХЛЕБ</span>
-    </div>
-  );
 
   return (
     <div className={styles.loading}>
