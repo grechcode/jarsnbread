ea8c9a5327809647f11f926f827eaf4d20cac4da
Gregory <grechka730@mail.ru>
2025-12-09 11:05:00 +0500

    update URLS

--- src/constants/URLS.js
+++ src/constants/URLS.js
@@ -1,4 +1,4 @@
 export const S3_STORAGE_URL =
-  "https://s3.twcstorage.ru/bac558d3-1b2424d7-b4c2-420a-8db9-b6144936832a";
+  "https://s3.twcstorage.ru/bac558d3-1b2424d7-b4c2-420a-8db9-b6144936832a/jarsnbread";
 
 export const IMAGES_URL = `${S3_STORAGE_URL}/images`;
